090ccaab6718b365ac6925d04c07154c5b24b7af
Scott Walsh <swalsh@securityscorecard.io>
2018-01-26 14:08:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# duckdns
+
+[![Build Status](https://travis-ci.org/invisiblethreat/duckdns.svg?branch=master)
+
+Golang client for updating DNS entries at https://duckdns.org
+
+## Usage
+
+```
+Usage of ./duckdns:
+  -c, --config string       Config file location (default "duckdns.yaml")
+  -d, --debug               Use debug mode
+  -n, --names stringSlice   Names to update with DuckDNS. Just the subdomain section. Use the flag multiple times to set multiple values.
+  -t, --token string        Token for updating DuckDNS
+  ```
+
+## Modes
+
+Preference for items is in the following order:
+
+* CLI
+* Environmental,
+* Configuration File
+
+If items are only partially complete, extra methods will be used to try and complete the needed values. If you only set the token on the CLI the name values
+will attempt be be filled from the environment variables, and finally the
+configuration file. This is also a useful strategy for overriding items that are
+set lower in the order of priority. Use the CLI for your names, and rely on the
+token from the configuration file.
+
+### CLI Only
+
+Pass all of the arguments in via CLI
+
+```bash
+
+duckdns -t <your token> -n name1 -n name2
+
+```
+
+### Environment Variables
+
+```bash
+
+export DUCK_TOKEN="<your token>"
+export DUCK_NAMES="name1 name2" #use space delimited names
+duckdns
+
+```
+
+### Configuration File
+
+`duckdns.yaml`
+
+```yaml
+
+---
+token: feedfeed-feed-feed-feed-feedfeedfeed
+domains:
+  - testdomain
+  - test-domain
+
+  ```
+
+```bash
+
+duckdns # uses duckdns.yaml in the same directory as the default
+# or
+duckdns -c /path/to/duckdns.yaml
+
+```
---
+++ main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/TV4/env"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+	yaml "gopkg.in/yaml.v2"
+)
+
+// Update contains everything that DuckDNS will need to update a record
+type Update struct {
+	Token string   `yaml:"token"`
+	Names []string `yaml:"domains"`
+}
+
+// CLIOptions are to set things via CLI
+type CLIOptions struct {
+	Debug bool
+	File  string
+	Token string
+	Names []string
+}
+
+// Valid checks that all parameters are set for an update
+func (u *Update) Valid() bool {
+	if len(u.Names) > 0 && u.Token != "" {
+		return true
+	}
+	return false
+}
+
+// GetConfigCLI sets the arguments for an update if they have been passed in on
+// the CLI
+func getConfigCLI(c CLIOptions) Update {
+	var u Update
+
+	u.Token = c.Token
+	logrus.Debugf("Set token from CLI to %s", c.Token)
+	u.Names = c.Names
+	logrus.Debugf("Set names from CLI to %s", strings.Join(c.Names, ", "))
+
+	return u
+}
+
+// GetConfigFile reads the config for DuckDNS
+func getConfigFile(existing *Update, file string) {
+
+	var update Update
+
+	yamlFile, err := ioutil.ReadFile(file)
+	if err != nil {
+		logrus.WithError(err).Debug("error reading file")
+		return
+	}
+	err = yaml.Unmarshal(yamlFile, &update)
+	if err != nil {
+		logrus.WithError(err).Debug("error unmarshaling YAML file")
+		return
+	}
+
+	// Set the token if it's not empty and doesn't already exist
+	if update.Token == "" {
+		logrus.Debugf("the token is empty after trying to parse %s", file)
+	} else if existing.Token == "" {
+		existing.Token = update.Token
+	}
+
+	// Set names to if they exist and value is not already set
+	if len(update.Names) == 0 {
+		logrus.Debugf("no names/subdomains specified to update from %s", file)
+	} else if len(existing.Names) == 0 {
+		existing.Names = update.Names
+	}
+
+}
+
+// GetConfigEnv is for reading items out of the environment if you didn't want
+// to set them on the CLI
+func getConfigEnv(u *Update) {
+	token := env.String("DUCK_TOKEN", "")
+	name := env.String("DUCK_NAMES", "")
+
+	// Set the token if not already set
+	if u.Token == "" {
+		u.Token = token
+		logrus.Debugf("Set token from environment to %s", token)
+	}
+
+	if len(u.Names) == 0 && name != "" {
+		// support DUCK_NAME="domain1 domain2" from the environment
+		u.Names = strings.Split(name, " ")
+
+		logrus.Debugf("Set names from environment to %s",
+			strings.Join(u.Names, ", "))
+	}
+}
+
+func makeUpdate(update Update) error {
+	logrus.Debugf("Dumping update params: %#v", update)
+	if !update.Valid() {
+		logrus.Fatal("Arguments not set for update!")
+	}
+	var errs []string
+	stub := "https://www.duckdns.org/update?domains="
+	tokenStub := "&token="
+	ipStub := "&ip="
+
+	for _, v := range update.Names {
+
+		url := fmt.Sprintf("%s%s%s%s%s", stub, v, tokenStub, update.Token, ipStub)
+		logrus.Debugf("Update string: %s", url)
+		res, err := http.Get(url)
+		if err != nil {
+			errs = append(errs, err.Error())
+		}
+
+		bodyBytes, _ := ioutil.ReadAll(res.Body)
+		res.Body.Close()
+
+		if strings.Contains(string(bodyBytes), "KO") {
+			return fmt.Errorf("error updating %s with DuckDNS", v)
+		}
+
+		logrus.Debugf("updated DuckDNS for name %s", v)
+
+	}
+
+	if len(errs) != 0 {
+		return errors.New(strings.Join(errs, "\n"))
+	}
+
+	return nil
+}
+
+func main() {
+	var cli CLIOptions
+
+	pflag.BoolVarP(&cli.Debug, "debug", "d", false, "Use debug mode")
+	pflag.StringVarP(&cli.File, "config", "c", "duckdns.yaml",
+		"Config file location")
+	pflag.StringSliceVarP(&cli.Names, "names", "n", nil,
+		"Names to update with DuckDNS. Just the subdomain section. "+
+			"Use the flag multiple times to set multiple values.")
+	pflag.StringVarP(&cli.Token, "token", "t", "",
+		"Token for updating DuckDNS")
+
+	pflag.Parse()
+
+	if cli.Debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	logrus.Debugf("Logging level: %s", logrus.GetLevel().String())
+
+	// CLI vars
+	update := getConfigCLI(cli)
+
+	// Set things that weren't set by the CLI
+	if !update.Valid() {
+		getConfigEnv(&update)
+	}
+
+	// File vars
+	if !update.Valid() {
+		getConfigFile(&update, cli.File)
+	}
+
+	err := makeUpdate(update)
+
+	if err != nil {
+		logrus.WithError(err).Fatal("error updating IP address")
+	}
+	logrus.Debug("IP address updated successfully")
+}
